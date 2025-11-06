574895db01dd56257e41fc25207c5fc408c614b7
Felícia Melany da Silva Cunha <feliciamelany01@gmail.com>
2025-11-06 10:33:21 -0300

    feat(api): Add de rota base

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import app from "./app.js";
+
+const PORT = 3000;
+
+
+app.get('/', (req, res,) =>{
+    res.status(200).send("Servidor de tarefas ON!")
+});
+
+
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Servidor rodando na porta: ${PORT}`)
+});
